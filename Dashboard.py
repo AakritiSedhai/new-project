6314c0fe14a82c8d27e25676d55c54d3bfdde2b1
AakritiSedhai <secondshare007@gmail.com>
2022-07-24 12:50:32 +0545

    projectfiles

---
+++ Dashboard.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+window = Tk()
+window.title("Main Page")
+window.geometry('640x420+350+150')
+window.resizable(False, False)
+window.config(bg="#0a3570")
+
+img1 = ImageTk.PhotoImage(file="Dashboard.png")
+my_canvas = Canvas(window, width=200, height=200, bg="#0a3570")
+my_canvas.create_image(0, 0, image=img1, anchor=NW)
+my_canvas.pack(fill="both", expand=True)
+window.mainloop()
